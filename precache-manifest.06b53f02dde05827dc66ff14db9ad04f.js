5a7d3755594a8d6dbb0d5b8e90df7de5bd75b090
lekpramual <lek.pramual@gmail.com>
2019-07-21 21:43:21 +0700

    Updates

---
+++ precache-manifest.06b53f02dde05827dc66ff14db9ad04f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84f3ce013125ee44c542a1d2684087f",
+    "url": "/barcode-react/index.html"
+  },
+  {
+    "revision": "2fa52e5c7808dc5db558",
+    "url": "/barcode-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a8c91a14e1024ebe691d",
+    "url": "/barcode-react/static/js/2.47b05639.chunk.js"
+  },
+  {
+    "revision": "2fa52e5c7808dc5db558",
+    "url": "/barcode-react/static/js/main.9f56d037.chunk.js"
+  },
+  {
+    "revision": "5cf802a80d811155bc71",
+    "url": "/barcode-react/static/js/runtime~main.8955b5bc.js"
+  }
+]);
