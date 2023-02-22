3fca2f7ce825b5253d51bf925d10ea5ce5c0f54c
lekpramual <lek.pramual@gmail.com>
2023-02-22 20:45:12 +0700

    Updates

---
+++ precache-manifest.ed206e46246a5506a4e0bea9b04b60e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625a3d446c1f134991f0d19016a3c8c3",
+    "url": "/barcode-react/index.html"
+  },
+  {
+    "revision": "750a429f11dd121c1542",
+    "url": "/barcode-react/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "522684247d23b1c9f329",
+    "url": "/barcode-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "750a429f11dd121c1542",
+    "url": "/barcode-react/static/js/2.37298001.chunk.js"
+  },
+  {
+    "revision": "522684247d23b1c9f329",
+    "url": "/barcode-react/static/js/main.a3fd3787.chunk.js"
+  },
+  {
+    "revision": "5cf802a80d811155bc71",
+    "url": "/barcode-react/static/js/runtime~main.8955b5bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/barcode-react/precache-manifest.0e6e3dd8437c986c85041194d0dddfeb.js"
+  "/barcode-react/precache-manifest.ed206e46246a5506a4e0bea9b04b60e2.js"
 );
 
 self.addEventListener('message', (event) => {
